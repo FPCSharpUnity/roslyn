52ba72df2c777c04d3cf1c0278c861234d3117a3
Evaldas Ciakas <evaldas757@gmail.com>
2022-06-15 00:56:51 +0300

    fix compilation

--- build_csc.ps1
+++ build_csc.ps1
@@ -1,9 +1,9 @@
 rm -r build
 mkdir build
-dotnet build -c Release .\src\Compilers\Server\VBCSCompiler\VBCSCompiler.csproj
-dotnet build -c Release .\src\Compilers\CSharp\csc\csc.csproj
-Copy-Item artifacts\bin\VBCSCompiler\Release\net6.0 -Destination build -Recurse
-Copy-Item artifacts\bin\csc\Release\net6.0 -Destination build -Recurse -Force
+dotnet build -c Release .\src\Compilers\Server\VBCSCompiler\AnyCpu\VBCSCompiler.csproj
+dotnet build -c Release .\src\Compilers\CSharp\csc\AnyCpu\csc.csproj
+# Copy-Item artifacts\bin\VBCSCompiler\Release\net6.0 -Destination build -Recurse
+# Copy-Item artifacts\bin\csc\Release\net6.0 -Destination build -Recurse -Force
 Copy-Item artifacts\bin\VBCSCompiler\Release\net5.0 -Destination build -Recurse
 Copy-Item artifacts\bin\csc\Release\net5.0 -Destination build -Recurse -Force
 Copy-Item artifacts\bin\Microsoft.CodeAnalysis\Release\netstandard2.0 -Destination build -Recurse -Force
