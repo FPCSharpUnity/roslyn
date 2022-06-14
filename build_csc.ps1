25aaf89510857b26a14e35661e163fafdfd7bfcc
Evaldas Ciakas <evaldas757@gmail.com>
2022-06-15 00:35:15 +0300

    Added net5.0 target

--- build_csc.ps1
+++ build_csc.ps1
@@ -4,5 +4,7 @@ dotnet build -c Release .\src\Compilers\Server\VBCSCompiler\VBCSCompiler.csproj
 dotnet build -c Release .\src\Compilers\CSharp\csc\csc.csproj
 Copy-Item artifacts\bin\VBCSCompiler\Release\net6.0 -Destination build -Recurse
 Copy-Item artifacts\bin\csc\Release\net6.0 -Destination build -Recurse -Force
+Copy-Item artifacts\bin\VBCSCompiler\Release\net5.0 -Destination build -Recurse
+Copy-Item artifacts\bin\csc\Release\net5.0 -Destination build -Recurse -Force
 Copy-Item artifacts\bin\Microsoft.CodeAnalysis\Release\netstandard2.0 -Destination build -Recurse -Force
 Copy-Item artifacts\bin\Microsoft.CodeAnalysis.CSharp\Release\netstandard2.0 -Destination build -Recurse -Force
