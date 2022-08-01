e6eec283e5546be9f3fb74436775a7a5c7599078
Evaldas Ciakas <evaldas757@gmail.com>
2022-08-01 12:23:25 +0300

    use net6.0 target

--- build_csc.ps1
+++ build_csc.ps1
@@ -2,9 +2,9 @@ rm -r build
 mkdir build
 dotnet build -c Release .\src\Compilers\Server\VBCSCompiler\AnyCpu\VBCSCompiler.csproj
 dotnet build -c Release .\src\Compilers\CSharp\csc\AnyCpu\csc.csproj
-# Copy-Item artifacts\bin\VBCSCompiler\Release\net6.0 -Destination build -Recurse
-# Copy-Item artifacts\bin\csc\Release\net6.0 -Destination build -Recurse -Force
-Copy-Item artifacts\bin\VBCSCompiler\Release\net5.0 -Destination build -Recurse
-Copy-Item artifacts\bin\csc\Release\net5.0 -Destination build -Recurse -Force
+Copy-Item artifacts\bin\VBCSCompiler\Release\net6.0 -Destination build -Recurse
+Copy-Item artifacts\bin\csc\Release\net6.0 -Destination build -Recurse -Force
+# Copy-Item artifacts\bin\VBCSCompiler\Release\net5.0 -Destination build -Recurse
+# Copy-Item artifacts\bin\csc\Release\net5.0 -Destination build -Recurse -Force
 Copy-Item artifacts\bin\Microsoft.CodeAnalysis\Release\netstandard2.0 -Destination build -Recurse -Force
 Copy-Item artifacts\bin\Microsoft.CodeAnalysis.CSharp\Release\netstandard2.0 -Destination build -Recurse -Force
